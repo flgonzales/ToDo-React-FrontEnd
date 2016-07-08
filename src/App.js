7728a6cebe2e3e73266fd7a1fad191f3e7e0f34d
Fabiano Gonzalez <fabianolgonzales@gmail.com>
2016-07-08 05:38:35 +0200

    MaterialUI-Checkbox

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import TodoList from './TodoList';
+import ReactDOM from 'react-dom';
+import lightBaseTheme from 'material-ui/styles/baseThemes/lightBaseTheme';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import MyAwesomeReactComponent from './MyAwesomeReactComponent';
+
+
 class App extends React.Component {
     render() {
         return (
-          <div>
+          <MuiThemeProvider muiTheme={getMuiTheme(lightBaseTheme)}>
             <TodoList/>
-          </div>
+          </MuiThemeProvider>
         );
     }
 }
